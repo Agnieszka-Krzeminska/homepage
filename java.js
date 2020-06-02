5b985edcca4c61bf40d4220b58617301eb64acb9
Agnieszka Krzeminska <agnieszka.krzeminskaa@gmail.com>
2020-06-02 15:25:17 +0200

    happy js

--- java.js
+++ java.js
@@ -5,3 +5,14 @@ console.log(name);
 console.log(age);
 
 console.log(`nazywam się ${name} i mam ${age} lat.`);
+
+const heading = document.querySelector(".herader-h2--js");
+
+console.log(heading);
+
+const play = document.querySelector(".zabawa-JS--js");
+
+play.innerHTML = `
+<h4>Zakup klatki</h4>
+Wimiary minimum 80cm, przy małych króliczku.  <br/>
+Najlepiej sprawdza się kojec. <br> `;
